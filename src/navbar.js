9dffc429a7c03c1c3212bd1ada87475e8f9a4068
Davide Baggio <davide.davide2405@gmail.com>
2022-12-06 11:00:53 +0100

    navbar

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+function Navbar(){
+	return(
+		<nav className='globalNav'>
+			<h1 className='globalSpan'>DWORDLE</h1>
+			<h3 className='menu'>ciao</h3>
+			<h3 className='menu'>come</h3>
+			<h3 className='menu'>va</h3>
+		</nav>
+	);
+}
+
+export default Navbar;
